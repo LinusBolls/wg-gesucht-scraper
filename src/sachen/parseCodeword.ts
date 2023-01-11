a715ba7de73625a9adbc161ad94e5fa42b13f7da
Linus BALLS <linus.bolls@code.berlin>
2023-01-11 12:58:29 +0100

    feat: further improve parseCodeword to avoid false positives

--- src/sachen/parseCodeword.ts
+++ src/sachen/parseCodeword.ts
@@ -1,7 +1,25 @@
+/**
+ * some listings include a passphrase, which is to be included
+ * in the application to show that the application text is not just being copy pasted.
+ * 
+ * the passphrase is almost always wrapped in "" quotes though, making it easy for us to parse.
+ * to filter out false positives, we only detect quoted expressions of three words or less,
+ * in a sentence that has to include either "word" or "wort".
+ * 
+ */
 export const parseCodeword = (str: string) => {
-  const quotedWord = /(?<=&quot;).*?(?=&quot;)/i;
+  /**
+   * match 3 or less words surrounded by quotes
+   */
+  const sentences = str.split(".")
 
-  if (quotedWord.test(str)) return str.match(quotedWord)![0]!;
+  for (const sentence of sentences) {
 
+    const quotedPhrase = /(?<=&quot;)(\w+\s?){1,3}?(?=&quot;)/i;
+
+    const quotedPhraseMatch = str.match(quotedPhrase)?.[0]
+
+    if (quotedPhraseMatch != null && /(word|wort)/i.test(sentence)) return quotedPhraseMatch
+  }
   return null;
 };
